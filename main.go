16d81c244acfdcdd0db6814212bdf0264aaf83b7
Bill Glover <bill@billglover.co.uk>
2016-05-22 11:20:53 +0100

    Confgirue logger

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	// prefix log entries with date and time (with microseconds) e.g.
+	// 2016/05/22 11:11:47.152342 location-api starting
+	log.SetFlags(log.Ldate | log.Lmicroseconds)
+	log.Println("location-api starting")
+}
