91b0871c62e0c93183c055bb68b1e774b96f4f16
Bill Glover <bill@billglover.co.uk>
2016-05-26 08:50:50 +0100

    Add first test to project

---
+++ handlers/location_test.go
@@ -0,0 +1,34 @@
+package location_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	var tests = []struct {
+		Fn func(rw http.ResponseWriter, req *http.Request)
+		S  int    // status
+		B  string //body
+	}{{
+		Fn: func(rw http.ResponseWriter, req *http.Request) {
+			rw.Write([]byte("Hello World!"))
+		},
+		S: 200,
+		B: "Hello World!",
+	}}
+
+	for _, test := range tests {
+		rw := httptest.NewRecorder()
+		test.Fn(rw, nil)
+		t.Log(rw.Code)
+		t.Log(rw.Body.String())
+		if test.S != rw.Code {
+			t.Fail()
+		}
+		if test.B != rw.Body.String() {
+			t.Fail()
+		}
+	}
+}
