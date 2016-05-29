144206fc76b77f12552018e7d703b39029ff1b03
Bill Glover <bill@billglover.co.uk>
2016-05-29 09:48:19 +0200

    Move tests to main package

--- main_test.go
+++ main_test.go
@@ -1,14 +1,18 @@
-package handlers_test
+package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/matryer/respond"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+)
+
+var (
+	dbName = "test"
+	dbUrl  = "localhost:27017"
 )
 
 var tests = []struct {
@@ -22,14 +26,14 @@ var tests = []struct {
 		Method:       "GET",
 		Path:         "/locations",
 		BodyContains: "",
-		Status:       http.StatusNotImplemented,
+		Status:       http.StatusNotFound,
 	},
 	{
 		Method:       "POST",
 		Path:         "/locations",
 		Body:         strings.NewReader(`{""}`),
 		BodyContains: "",
-		Status:       http.StatusCreated,
+		Status:       http.StatusNotImplemented,
 	},
 	{
 		Method:       "GET",
@@ -41,30 +45,35 @@ var tests = []struct {
 
 func TestAll(t *testing.T) {
 	assert := assert.New(t)
-	server := httptest.NewServer(&myhandler{})
+
+	// create a DB Session
+	// TODO: this needs to be able to take a database name
+	dbSession := CreateDbSession(dbUrl)
+	defer dbSession.Close()
+
+	// create a Router
+	router := apiRouter(dbSession)
+
+	// create a test server
+	server := httptest.NewServer(router)
 	defer server.Close()
+
+	// execute our tests
 	for _, test := range tests {
+
+		// create and execute an HTTP request
 		r, err := http.NewRequest(test.Method, server.URL+test.Path, test.Body)
 		assert.NoError(err)
-		// call handler
 		response, err := http.DefaultClient.Do(r)
 		assert.NoError(err)
+
+		// extract the body of the response
 		actualBody, err := ioutil.ReadAll(response.Body)
 		assert.NoError(err)
+
 		// make assertions
-		assert.Contains(string(actualBody), test.BodyContains, "body")
-		assert.Equal(test.Status, response.StatusCode, "status code")
+
+		assert.Contains(string(actualBody), test.BodyContains, "%s %s %s", test.Method, test.Path, "\n\tunexpected body returned")
+		assert.Equal(test.Status, response.StatusCode, "%s %s %s", test.Method, test.Path, "\n\tunexpected status code in response")
 	}
-}
-
-func (h *myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	respond.WithStatus(w, r, http.StatusOK)
-}
-
-type myhandler struct{}
-
-func main() {
-	var tests []testing.InternalTest
-	tests = append(tests, testing.InternalTest{Name: "TestAll", F: TestAll})
-	testing.Main(func(pat, str string) (bool, error) { return true, nil }, tests, nil, nil)
 }
