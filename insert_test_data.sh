c11cc9094737929f851f30926e6b230d152187d9
Bill Glover <bill@billglover.co.uk>
2016-05-31 21:11:11 +0100

    Add additional test data

--- insert_test_data.sh
+++ insert_test_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mongo test --eval 'db.Locations.insert({_id: ObjectId("574cb30f4bf4c8f0c6a056e8"), latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "test location"});'
+mongo test --eval 'db.Locations.insert({_id: ObjectId("574cb30f4bf4c8f0c6a056e8"), latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "test location 1"});'
+mongo test --eval 'db.Locations.insert({_id: ObjectId("574de23b5f810df11cad3498"), latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "test location 2"});'
