a7e26e9db282451f3905984b17207ce9666d4717
Bill Glover <bill@billglover.co.uk>
2016-06-14 07:05:31 +0100

    Align bson and json name for altitude

--- models/location.go
+++ models/location.go
@@ -9,7 +9,7 @@ type Location struct {
 	Id                 bson.ObjectId `json:"id" bson:"_id"`
 	Latitude           float64       `json:"latitude,omitempty" bson:"latitude,omitempty"`
 	Longitude          float64       `json:"longitude,omitempty" bson:"longitude,omitempty"`
-	Altitude           float64       `json:"altitude,omitempty" bson:"alt,omitempty"`
+	Altitude           float64       `json:"altitude,omitempty" bson:"altitude,omitempty"`
 	HorizontalAccuracy float64       `json:"horizontalAccuracy,omitempty" bson:"horizontalAccuracy,omitempty"`
 	VerticalAccuracy   float64       `json:"verticalAccuracy,omitempty" bson:"verticalAccuracy,omitempty"`
 	DeviceTime         time.Time     `json:"devicetime,omitempty" bson:"devicetime,omitempty"`
