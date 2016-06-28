9533d81840235263da740b2be797073d8c1ae7eb
Bill Glover <bill@billglover.co.uk>
2016-06-28 06:34:46 +0100

    Add time_from and time_to as optional query parameters to GET /locations

--- handlers/location.go
+++ handlers/location.go
@@ -24,10 +24,36 @@ func today() time.Time {
 func LocationsGet(w http.ResponseWriter, r *http.Request) {
 	page, _     := strconv.Atoi(r.URL.Query().Get("page"))		// on error page is set to 0
 	per_page, _ := strconv.Atoi(r.URL.Query().Get("per_page"))	// on error per_page is set to 0
+	time_from_string := r.URL.Query().Get("time_from")
+	time_to_string := r.URL.Query().Get("time_to")
+
+	// construct a search query
+	f := bson.M{}
+	hAccuracy := bson.M{"$lt": 100}
+	f["horizontalAccuracy"] = hAccuracy
+	f["description"] = "location"
+
+	time_range := bson.M{}
+
+	if time_from_string != "" {
+		time_from, _ := time.Parse(time.RFC3339, time_from_string)	
+		log.Println("time_from: ", time_from)
+		time_range["$gte"] = time_from
+	}
+	if time_to_string != "" {
+		time_to, _ := time.Parse(time.RFC3339, time_to_string)
+		log.Println("time_to: ", time_to)
+		time_range["$lte"] = time_to
+	}
+	if time_to_string != "" || time_from_string != "" {
+		f["devicetime"] = time_range
+	}
+	log.Println(f)
+
 
 	db := context.Get(r, "db").(*mgo.Session)
 	l := []models.Location{}
-	err := db.DB("").C("Locations").Find(bson.M{"horizontalAccuracy": bson.M{"$lt": 100}, "description": "location", "devicetime": bson.M{"$gte": today()}}).Sort("-_id").Skip((page-1)*per_page).Limit(per_page).All(&l)
+	err := db.DB("").C("Locations").Find(f).Sort("-_id").Skip((page-1)*per_page).Limit(per_page).All(&l)
 
 	if err != nil {
 		log.Println(err.Error())
--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"net/http/httptest"
 	"strings"
 	"testing"
@@ -97,7 +98,7 @@ func TestPostInvalidLocation(t *testing.T) {
 func TestPostValidLocation(t *testing.T) {
 	assert := assert.New(t)
 
-	userJson := `[{"latitude":1.1111,"longitude":2.2222,"altitude":3.3333,"horizontalAccuracy":4.4444,"verticalAccuracy":5.5555,"devicetime":"2016-06-01T07:00:00Z","description":"test location 3"}]`
+	userJson := `[{"latitude":1.1111,"longitude":2.2222,"altitude":3.3333,"horizontalAccuracy":4.4444,"verticalAccuracy":5.5555,"devicetime":"2016-06-01T07:00:00Z","description":"location"}]`
     reader := strings.NewReader(userJson)
 
 	req, err := http.NewRequest("POST", serverUrl + "/locations", reader)
@@ -131,4 +132,53 @@ func TestGetOneLocation(t *testing.T) {
 	assert.Contains(string(body), "id", "%s", "unexpected body returned")
 	assert.Contains(string(body), validLocationId, "%s", "unexpected body returned")
 	assert.Equal(http.StatusOK, res.StatusCode, "%s", "unexpected status code")
+}
+
+func TestGetLocationsFromTo(t *testing.T) {
+	assert := assert.New(t)
+
+	time_from_string := url.QueryEscape("2016-06-01T06:00:00Z")
+	time_to_string := url.QueryEscape("2016-06-01T07:00:30Z")
+	req, err := http.NewRequest("GET", serverUrl + "/locations?time_from=" + time_from_string + "&time_to=" + time_to_string, nil)
+	assert.NoError(err)
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(err)
+
+	assert.Contains(string(body), "latitude", "%s", "unexpected body returned")
+	assert.Contains(string(body), "longitude", "%s", "unexpected body returned")
+	assert.Equal(http.StatusOK, res.StatusCode, "%s", "unexpected status code")
+
+	var jsonObjs interface{}
+	errJson := json.Unmarshal([]byte(body), &jsonObjs)
+	objSlice, ok := jsonObjs.([]interface{})
+	assert.Equal(true, ok, "cannot convert response to JSON object")
+	assert.NoError(errJson)
+	assert.NotEqual(0, len(objSlice), "%s", "unexpected number of objects returned")
+}
+
+func TestGetLocationsFrom(t *testing.T) {
+	assert := assert.New(t)
+
+	time_from_string := url.QueryEscape("2016-06-01T06:00:00Z")
+	req, err := http.NewRequest("GET", serverUrl + "/locations?time_from=" + time_from_string, nil)
+	assert.NoError(err)
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(err)
+
+	assert.Contains(string(body), "latitude", "%s", "unexpected body returned")
+	assert.Contains(string(body), "longitude", "%s", "unexpected body returned")
+	assert.Equal(http.StatusOK, res.StatusCode, "%s", "unexpected status code")
+
+	var jsonObjs interface{}
+	errJson := json.Unmarshal([]byte(body), &jsonObjs)
+	objSlice, ok := jsonObjs.([]interface{})
+	assert.Equal(true, ok, "cannot convert response to JSON object")
+	assert.NoError(errJson)
+	assert.NotEqual(0, len(objSlice), "%s", "unexpected number of objects returned")
 }
