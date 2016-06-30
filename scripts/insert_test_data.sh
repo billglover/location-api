1f17e7e599f2c5ce6d320f48c7e64347ec402d49
Bill Glover <bill@billglover.co.uk>
2016-06-30 06:15:52 +0100

    Add test data for Visits

--- scripts/insert_test_data.sh
+++ scripts/insert_test_data.sh
@@ -7,3 +7,8 @@ mongo test --eval 'db.Locations.insert({latitude: 1.1111, longitude: 2.2222, alt
 mongo test --eval 'db.Locations.insert({latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "location"});'
 mongo test --eval 'db.Locations.insert({latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "location"});'
 mongo test --eval 'db.Locations.insert({latitude: 1.1111, longitude: 2.2222, altitude: 3.3333, horizontalAccuracy: 4.4444, verticalAccuracy: 5.5555, description: "location"});'
+
+
+mongo test --eval 'db.Visits.insert({latitude: 1.1111, longitude: 2.2222, horizontalAccuracy: 3.3333, arrivalTime: new ISODate("2016-06-01T06:00:00Z"), departureTime: new ISODate("2016-06-01T07:00:00Z"), description: "visit"});'
+mongo test --eval 'db.Visits.insert({latitude: 1.1111, longitude: 2.2222, horizontalAccuracy: 3.3333, arrivalTime: new ISODate("2016-06-02T06:00:00Z"), departureTime: new ISODate("2016-06-02T07:00:00Z"), description: "visit"});'
+mongo test --eval 'db.Visits.insert({latitude: 1.1111, longitude: 2.2222, horizontalAccuracy: 3.3333, arrivalTime: new ISODate("2016-06-03T06:00:00Z"), departureTime: new ISODate("2016-06-03T07:00:00Z"), description: "visit"});'
